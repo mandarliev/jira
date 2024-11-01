72ddfcb94ffd2e63bd445a45c50c7cc9eaa6b332
Dimitar <dimitar.mandarliev@gmail.com>
2024-11-01 23:03:07 +0200

    revert changes

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -13,8 +13,8 @@ export async function signUpWithGithub() {
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
@@ -23,12 +23,12 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
-  // const origin = headers().get("origin");
+  const origin = headers().get("origin");
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
